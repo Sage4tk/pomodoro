1ced0d9c18e861c2d9210755e7a56ac955ebea8c
timothy timbol <ttimbol08@gmail.com>
2021-06-10 23:19:31 +0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+pomodorTimer = (timeMinute, restMinute) => {
+    let currentTime = timeMinute * 60 + 1;
+    let work = true;
+
+    const timer = setInterval(() => {
+        if (currentTime <= 1) {
+            if (work === true) {
+                currentTime = restMinute * 60 + 1;
+            } else {
+                currentTime = timeMinute * 60 + 1;
+            }
+            work = !work;
+        }
+        
+        currentTime -= 1;
+    }, 1000)
+}
+
+pomodorTimer(0.5, 0.1)
